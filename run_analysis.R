e3480c0a93ce4db345aaf2d6339ceccff6ad3eca
Jeremi Summerhill <30322753+JeremiShane@users.noreply.github.com>
2017-08-31 13:53:03 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,77 +1,63 @@
-## get data and produce tidy data set for future analysis
-## data collected from smart phone accelerometer data
-## purpose of original data set to recognize human activity
+## Created by: Jeremi Summerhill
+## updated 08/30/2017
+## Coursera Data Science - Getting and Cleaning Data Assignment
+## Description: Get and Tidy dataset from research to identify human movements
+## from accelerometer and gyro on samsung mobile
+## For full description see CodeBook.md, and Readme.md
 
-## the features data represent calculated measurements from the 
-##..acceleraometer and gyro readings.
-##WE will ONLY extract data that contain the mean and standard deviation 
-##...measures from the features
-
-## Then the final data set will produce the average of these measures by
-##..subject and activity.  Resulting in only one row of data per subject
-##..and activity
-
-## the data comes split into test and training data sets
-## Note - a requirement for this analysis is to combine the test and training data
-
-## each test and train data set has its own set of files:
-## one file contains the human subject identifiers for each row element
-## and one file contains 561 calculated measurements (features) for each row
-## one file contains the activity label identifiers for each row element
-## each of the files contains 2947 rows of data
-## one file contains the activity id and label for each activity
-## one file contains id and label for each feature
 
 getclean <- function(datadir=getwd(), writedir=getwd()){
-  library(dplyr)
+        library(plyr)  ## load library plyr before dplyr
+        library(dplyr)
         
         ##this function accepts a directory to retrieve data, and
         ##...a directory to write data
-        
-        ## this funciton will write out the following files:
+        ## writes the following files to writedir
+        ## 
         ## “tidy_avg_mean_and_std_features.txt”
         ## "mean_and_std_features.txt"
+        ## "descriptive_mean_and_std_features.txt"
         
         ## this funciton returns df which is 
         ##...same as “tidy_avg_mean_and_std_features.txt”
-        ##...with the addition of acticity label in the df
         
         ## read the feature labels and idenfifiers into R
-        d <- paste(datadir, "features.txt", sep="") ## set file directory and name
+        d <- paste(datadir, "/features.txt", sep="") ## set file directory and name
         featureLabels <- read.table(d, col.names=c("id", "feature"), strip.white=TRUE)
-        vFeatures <- featureLabels$feature ## vector of labels
+        vFeatures <- featureLabels$feature ## vector of feature labels
         
         ## read the activity labels and identifiers
-        d <- paste(datadir, "activity_labels.txt", sep="")
-        activityLabels <- read.table(d, col.names=c("id", "activity"), strip.white=TRUE)
-        vActivity <- activityLabels$activity ## vector of labels
+        d <- paste(datadir, "/activity_labels.txt", sep="")
+        dfactivityLabels <- read.table(d, col.names=c("activity.id", "activity"), strip.white=TRUE)
+        ##vActivity <- activityLabels$activity ## vector of labels
+        ##return(dfactivityLabels)
         
         ## read the test features into R
-        d <- paste(datadir, "X_test.txt", sep="")
-        testFeatures <- read.table(d, strip.white=TRUE, col.names=vFeatures)
+        d <- paste(datadir, "/X_test.txt", sep="")
+        testFeatures <- read.table(d, strip.white=TRUE, col.names=vFeatures, colClasses = "numeric")
         
         ## read the train features into R
-        d <- paste(datadir, "X_train.txt", sep="")
-        trainFeatures <- read.table(d, strip.white=TRUE, col.names=vFeatures)
+        d <- paste(datadir, "/X_train.txt", sep="")
+        trainFeatures <- read.table(d, strip.white=TRUE, col.names=vFeatures, colClasses = "numeric")
         
         ## read the test subject identifiers into R
-        d <- paste(datadir, "subject_test.txt", sep="")
-        testSubjectId <- read.table(d, strip.white=TRUE, col.names="id")
+        d <- paste(datadir, "/subject_test.txt", sep="")
+        testSubjectId <- read.table(d, strip.white=TRUE, col.names="id", colClasses = "numeric")
         vTestSubject <- testSubjectId$id
         
         ## read the train subject identifiers into R
-        d <- paste(datadir, "subject_train.txt", sep="")
-        trainSubjectId <- read.table(d, strip.white=TRUE, col.names="id")
+        d <- paste(datadir, "/subject_train.txt", sep="")
+        trainSubjectId <- read.table(d, strip.white=TRUE, col.names="id", colClasses = "numeric")
         vTrainSubject <- trainSubjectId$id
         
         ## read the test activity idenfifiers into R
-        d <- paste(datadir, "y_test.txt", sep="")
-        testActivityId <- read.table(d, strip.white=TRUE, col.names="id")
+        d <- paste(datadir, "/y_test.txt", sep="")
+        testActivityId <- read.table(d, strip.white=TRUE, col.names="id", colClasses = "numeric")
         vTestActivity <- testActivityId$id
         
         ## read the train activity identifiers into R
-        d <- paste(datadir, "y_train.txt", sep="")
-        trainActivityId <- read.table(d, strip.white=TRUE, col.names="id")
+        d <- paste(datadir, "/y_train.txt", sep="")
+        trainActivityId <- read.table(d, strip.white=TRUE, col.names="id", colClasses = "numeric")
         vTrainActivity <- trainActivityId$id
         
         ## now need to add variables for the activity and subject ids 
@@ -86,23 +72,68 @@ getclean <- function(datadir=getwd(), writedir=getwd()){
         vSubjects <- allFeatures$subject_id
         vActivity <- allFeatures$activity_id
         
-        ## extract the mean and standard deviation measures
-        meanstdFeatures <- allFeatures[,grep("mean|std", names(allFeatures), value=TRUE)]
-        x <- length(meanstdFeatures) ## count of columns
+        ## extract the mean and standard deviation feature measures
+        dfmeanstdFeatures <- allFeatures[,grep("mean|std", names(allFeatures), value=TRUE)]
+        x <- length(dfmeanstdFeatures) ## count of columns
+        
         ## write a file with our mean and std feature names
         d <- paste(writedir, "mean_and_std_features.txt", sep="")
-        write.table(names(meanstdFeatures), d, sep=" ", col.names=FALSE, row.names=1:x, quote=FALSE)
+        write.table(names(dfmeanstdFeatures), d, sep=" ", col.names=FALSE, row.names=1:x, quote=FALSE)
         
-        meanstdFeatures$activity_id <- vActivity
-        meanstdFeatures$subject_id <- vSubjects
+        ## add columns for activity id and subject id
+        dfmeanstdFeatures$activity.id <- vActivity
+        dfmeanstdFeatures$subject.id <- vSubjects
         
         ## now to average the measures by subject, and activity
-        df <- ddply(meanstdFeatures, c("subject_id", "activity_id"), colMeans)
+        ## ddply will take the column means and group the data frame by subject and activity
+        ## so we will only be left with one row per subject and activity and 
+        ## each column will contain the mean of each measure taken for that subject and activity
+        df <- ddply(dfmeanstdFeatures, c("subject.id", "activity.id"), colMeans)
         
-        ## adding in the activity_label as a column
-        df <- merge(df, activityLabels, by.x="activity_id", by.y="id")
+        ## adding in the activity label as a column
+        df <- merge(df, dfactivityLabels, by.x="activity.id", by.y="activity.id", sort=FALSE)
+        
+        vDescriptiveMeanStdFeatures <- colnames(df)
+        ## making a new vector for the new descriptive column names for tidy dataset
+        vDescriptiveMeanStdFeatures <- gsub("\\.","",vDescriptiveMeanStdFeatures)
+        vDescriptiveMeanStdFeatures <- gsub("^t","time",vDescriptiveMeanStdFeatures)
+        vDescriptiveMeanStdFeatures <- gsub("^f","frequency",vDescriptiveMeanStdFeatures)
+        vDescriptiveMeanStdFeatures <- gsub("Freq","frequency",vDescriptiveMeanStdFeatures)
+        vDescriptiveMeanStdFeatures <- gsub("BodyAcc","\\.body\\.acceleration",vDescriptiveMeanStdFeatures)
+        vDescriptiveMeanStdFeatures <- gsub("GravAcc","\\.gravity\\.acceleration",vDescriptiveMeanStdFeatures)
+        vDescriptiveMeanStdFeatures <- gsub("GravityAcc","\\.gravity\\.acceleration",vDescriptiveMeanStdFeatures)
+        vDescriptiveMeanStdFeatures <- gsub("BodyBody","\\.body",vDescriptiveMeanStdFeatures)
+        vDescriptiveMeanStdFeatures <- gsub("Body","\\.body",vDescriptiveMeanStdFeatures)
+        vDescriptiveMeanStdFeatures <- gsub("Gyro","\\.gyroscope",vDescriptiveMeanStdFeatures)
+        vDescriptiveMeanStdFeatures <- gsub("Mag","\\.magnitude",vDescriptiveMeanStdFeatures)
+        vDescriptiveMeanStdFeatures <- gsub("Jerk","\\.jerk",vDescriptiveMeanStdFeatures)
+        vDescriptiveMeanStdFeatures <- gsub("std","\\.standarddeviation",vDescriptiveMeanStdFeatures)
+        vDescriptiveMeanStdFeatures <- gsub("X","\\.xdirection",vDescriptiveMeanStdFeatures)
+        vDescriptiveMeanStdFeatures <- gsub("Y","\\.ydirection",vDescriptiveMeanStdFeatures)
+        vDescriptiveMeanStdFeatures <- gsub("Z","\\.zdirection",vDescriptiveMeanStdFeatures)
+        vDescriptiveMeanStdFeatures <- gsub("mean","\\.mean",vDescriptiveMeanStdFeatures)
+        vDescriptiveMeanStdFeatures <- gsub("activityid","activity\\.id",vDescriptiveMeanStdFeatures)
+        vDescriptiveMeanStdFeatures <- gsub("subjectid","subject\\.id",vDescriptiveMeanStdFeatures)
+        
+        ## replace the column names with the descriptive column names
+        colnames(df) <- vDescriptiveMeanStdFeatures
+        ##df$activity.id <-- NULL
+        df <- df %>% select(subject.id, activity.id, activity, everything())
+        df <- arrange(df, subject.id)
+        
+        ## write a file of our descriptive feature column names
+        d <- paste(writedir, "/descriptive_mean_and_std_features.txt", sep="")
+        x <- length(df)
+        write.table(names(df), d, sep=" ", col.names=FALSE, row.names=1:x, quote=FALSE)
+        
         ##write our final tidy file out
-        d <- paste(writedir, "tidy_avg_mean_and_std_features.txt", sep="")
+        d <- paste(writedir, "/tidy_avg_mean_and_std_features.txt", sep="")
         write.table(df, d, sep=" ", col.names=TRUE, row.names=FALSE, quote=FALSE)
-        df
+        
+        d <- paste(writedir, "/tidy.csv", sep="")
+        write.csv(df, d, quote=FALSE)
+        
+        ## output number of columns and rows
+        rdf <- c(x, nrow(df))
+        rdf
 }
